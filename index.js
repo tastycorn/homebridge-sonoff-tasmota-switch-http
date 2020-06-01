ce2469bdba2145ce76f9fae1ae94de304aa6735a
tastycorn <ryan.peterson@ualberta.net>
2020-05-31 21:36:16 -0600

    Update index.js
    Reverted back to a single accessory type, making it a switch.

--- index.js
+++ index.js
@@ -5,30 +5,28 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   
-  homebridge.registerAccessory("homebridge-sonoff-tasmota-http", "SonoffTasmotaHTTP", SonoffTasmotaHTTPAccessory);
+  homebridge.registerAccessory("homebridge-sonoff-tasmota-switch-http", "SonoffTasmotaSwitchHTTP", SonoffTasmotaSwitchHTTPAccessory);
 }
 
-function SonoffTasmotaHTTPAccessory(log, config) {
+function SonoffTasmotaSwitchHTTPAccessory(log, config) {
   this.log = log;
   this.config = config;
   this.name = config["name"]
-  this.type = config["type"]
   this.relay = config["relay"] || ""
   this.hostname = config["hostname"] || "sonoff"
   this.password = config["password"] || "";
   
-  if (this.type = "outlet") this.service = new Service.Outlet(this.name);
-  if (this.type = "switch") this.service = new Service.Switch(this.name);
+  this.service = new Service.Switch(this.name);
   
   this.service
   .getCharacteristic(Characteristic.On)
   .on('get', this.getState.bind(this))
   .on('set', this.setState.bind(this));
   
-  this.log("Sonoff Tasmota HTTP Initialized")
+  this.log("Sonoff Tasmota Switch HTTP Initialized")
 }
 
-SonoffTasmotaHTTPAccessory.prototype.getState = function(callback) {
+SonoffTasmotaSwitchHTTPAccessory.prototype.getState = function(callback) {
   var that = this
   request("http://" + that.hostname + "/cm?user=admin&password=" + that.password + "&cmnd=Power" + that.relay, function(error, response, body) {
     if (error) return callback(error);
@@ -45,7 +43,7 @@ SonoffTasmotaHTTPAccessory.prototype.getState = function(callback) {
   })
 }
 
-SonoffTasmotaHTTPAccessory.prototype.setState = function(toggle, callback) {
+SonoffTasmotaSwitchHTTPAccessory.prototype.setState = function(toggle, callback) {
   var newstate = "%20Off"
   if (toggle) newstate = "%20On"
   var that = this
@@ -64,6 +62,6 @@ SonoffTasmotaHTTPAccessory.prototype.setState = function(toggle, callback) {
   })
 }
 
-SonoffTasmotaHTTPAccessory.prototype.getServices = function() {
+SonoffTasmotaSwitchHTTPAccessory.prototype.getServices = function() {
   return [this.service];
 }
